953075fad4c415266eccdaf59a1232898dea50c9
ShinyaSano <shinya.sano.m@gmail.com>
2015-08-23 03:07:17 +0900

    障害物(風船)追加、それに伴い炎パーティクルとPartyPackのAssetを追加

---
+++ Assets/Scripts/Balloon.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Balloon : MonoBehaviour {
+
+	[SerializeField]
+	GameObject _explosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if(collider.gameObject.tag == "Player")
+		{
+			//Hit!
+
+			//Explosion
+			if(_explosion != null)
+			{
+				GameObject explosion = Instantiate(_explosion); 
+				Vector3 setPos = this.gameObject.transform.localPosition + this.gameObject.transform.parent.position;
+				setPos.y += 2;
+				_explosion.transform.position = setPos;
+			}
+
+			//Destroy This with Parent
+			Destroy(this.gameObject.transform.parent.gameObject); 
+		}
+	}
+}
