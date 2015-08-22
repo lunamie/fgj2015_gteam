efdf7598e2af83f6b95c3169f5fcffecbd13135d
AyakiOgino <Ayaki@AyakiOgino-no-MacBook-Pro.local>
2015-08-22 18:14:04 +0900

    仮の障害物実装

---
+++ Assets/Scripts/Obstruct.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstruct : MonoBehaviour {
+
+	public float _fVelocity ;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 3.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0.0f, 0.0f, -_fVelocity);
+	}
+}
