fff8cf7706ada4937aac48a7c7e0b14e82ba0142
AyakiOgino <Ayaki@AyakiOgino-no-MacBook-Pro.local>
2015-08-23 10:47:56 +0900

    仮タイトルシーン実装　GUIによる接続処理を削除し自動接続を実装

---
+++ Assets/Scripts/ClientController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class ClientController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var netManage = GameObject.Find( "NetworkManager" ).gameObject.GetComponent<NetworkManager> ();
+		netManage.StartClient ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
+
 
 public class GameController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		var netManage = GameObject.Find ("NetworkManager").gameObject.GetComponent<NetworkManager> ();
+		netManage.StartServer ();
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if( Input.GetKey( KeyCode.Escape ) ) {
